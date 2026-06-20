92915902e001abfae33cc48aac53c3c2bba97d24
Mohammad Arman <mohdarman7043@gmail.com>
2026-06-20 12:05:16 +0530

    fix: conditional base path for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/connectsphere/',
+  base: process.env.GITHUB_ACTIONS ? '/connectsphere/' : '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit
